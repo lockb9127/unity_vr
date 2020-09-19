165e550bd4ba48094722df4a4b952c9d3b375dc6
masayushi <4a1e0010@stust.edu.tw>
2020-09-19 20:15:49 +0800

    8

---
+++ New Unity Project/Assets/腳本/pillar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pillar : MonoBehaviour
+{
+    [Header("旋轉角度"), Range(0,100)]
+    public float angle = 90;
+    [Header("旋轉速度"), Range(0, 100)]
+    public float speed = 10f;
+
+    public void StartRotate()
+    {
+        StartCoroutine(Rotate());
+    }
+
+    public IEnumerator Rotate()
+    {
+        while(transform.rotation !=Quaternion.Euler(0, angle,0))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, angle, 0), Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+
+
+
+
+
+
+}
